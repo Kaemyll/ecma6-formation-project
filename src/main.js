e9f85cbe649a58fbe6fc023667ff2ae176df2786
Garnier David <david.garnier483@orange.fr>
2021-09-07 16:26:59 +0200

    dev: functions added on buttons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import "./style.css";
+import { Menu } from "./Menu";
 
-const trucBidule = "helloxxxx";
-
-console.log(trucBidule);
+const menu = new Menu();
+menu.add("button.addLine", () => {
+  console.log("about to add a line");
+});
+menu.add("button.addCircle", () => {
+  console.log("about to add a circle");
+});
+menu.add("button.eraseObject", () => {
+  console.log("about to erase the selected object");
+});
