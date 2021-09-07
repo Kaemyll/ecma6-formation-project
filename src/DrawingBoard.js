ee10b41d6883088d33008b62961c6c3a0bb40d62
Garnier David <david.garnier483@orange.fr>
2021-09-07 16:45:08 +0200

    dev: added DrawingBoard

---
+++ src/DrawingBoard.js
@@ -0,0 +1,17 @@
+export const STATE = {
+  DEFAULT: "default",
+  INSERTION: "insertion",
+  EDITION: "edition",
+  SELECTION: "selection",
+};
+
+export class DrawingBoard {
+  set state(val) {
+    console.log("val: ", val);
+    const elt = document.querySelector(".status");
+    elt.innerHTML = val;
+  }
+  constructor() {
+    this.state = STATE.DEFAULT;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import "./style.css";
 import { Menu } from "./Menu";
+import { DrawingBoard, STATE } from "./DrawingBoard";
 
+const board = new DrawingBoard();
 const menu = new Menu();
+
 menu.add("button.addLine", () => {
   console.log("about to add a line");
+  board.state = STATE.INSERTION;
 });
 menu.add("button.addCircle", () => {
   console.log("about to add a circle");
+  board.state = STATE.SELECTION;
 });
 menu.add("button.eraseObject", () => {
   console.log("about to erase the selected object");
+  board.state = STATE.DEFAULT;
 });
